c43e56f5e13c78830edf34ef7b85b054dc149e14
moranqingchen <184918211@qq.com>
2016-04-14 15:27:33 +0800

    查看简历
    查看简历

---
+++ js/allHave.js
@@ -0,0 +1,25 @@
+document.getElementById("nav-home").addEventListener('tap', function() {
+
+			mui.openWindow({
+				url: 'main.html',
+				id: 'main'
+			})
+})
+document.getElementById("nav-job").addEventListener('tap', function() {
+			mui.openWindow({
+				url: 'job.html',
+				id: 'job'
+			})
+})
+document.getElementById("nav-message").addEventListener('tap', function() {
+			mui.openWindow({
+				url: 'message.html',
+				id: 'message'
+			})
+})
+document.getElementById("nav-setting").addEventListener('tap', function() {
+			mui.openWindow({
+				url: 'setting.html',
+				id: 'setting'
+			})
+})
